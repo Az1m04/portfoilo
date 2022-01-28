5a5e2339cf66c0f204105927ab622f2da1cf295d
Azim <azim.babu@simbaquartz.com>
2022-01-29 00:09:27 +0530

    added project

--- pages/Work/index.js
+++ pages/Work/index.js
@@ -121,21 +121,126 @@ function Work() {
             </div>
           </div>
         </div>
-        <div className="flex justify-between leading-3 text-gray-400 text-2xl invisible">
+        <div className="flex justify-between leading-3 text-gray-400 text-2xl">
           <div
             className="bg-gradient-to-r from-gray-700 to-gray-900 rounded-md shadow-md p-5 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
             style={{ height: "280px", width: "400px" }}
           >
-            <div className="text-gray-300 font-semibold"></div>
+            <div className="text-yellow-600 font-semibold">CryptoPunk App</div>
+            <div className="mt-5 text-base">
+              NFT Marketplace using third web api .
+            </div>
+            <div className="mt-5">
+              <div>
+                <div className="text-base font-semibold mb-2">Tools used:</div>
+                <div className="flex space-x-2">
+                  <div className=" h-8 p-1 rounded-md bg-gray-800">
+                    <svg
+                      width="60"
+                      height="24"
+                      viewBox="0 0 148 90"
+                      version="1.1"
+                      xmlns="http://www.w3.org/1999/xlink"
+                      style={{
+                        transform: "translateX(4%)",
+                        shapeRendering: "auto",
+                      }}
+                    >
+                      <path
+                        d="M34.992 23.495h27.855v2.219H37.546v16.699h23.792v2.219H37.546v18.334h25.591v2.219H34.992v-41.69zm30.35 0h2.96l13.115 18.334 13.405-18.334L113.055.207 83.1 43.756l15.436 21.429H95.46L81.417 45.683 67.316 65.185h-3.018L79.85 43.756 65.343 23.495zm34.297 2.219v-2.219h31.742v2.219h-14.623v39.47h-2.554v-39.47H99.64zM.145 23.495h3.192l44.011 66.003L29.16 65.185 2.814 26.648l-.116 38.537H.145v-41.69zm130.98 38.801c-.523 0-.914-.405-.914-.928 0-.524.391-.929.913-.929.528 0 .913.405.913.929 0 .523-.385.928-.913.928zm2.508-2.443H135c.019.742.56 1.24 1.354 1.24.888 0 1.391-.535 1.391-1.539v-6.356h1.391v6.362c0 1.808-1.043 2.849-2.77 2.849-1.62 0-2.732-1.01-2.732-2.556zm7.322-.08h1.379c.118.853.95 1.395 2.149 1.395 1.117 0 1.937-.58 1.937-1.377 0-.685-.521-1.097-1.708-1.377l-1.155-.28c-1.62-.38-2.36-1.166-2.36-2.487 0-1.602 1.304-2.668 3.26-2.668 1.82 0 3.15 1.066 3.23 2.58h-1.354c-.13-.828-.85-1.346-1.894-1.346-1.1 0-1.832.53-1.832 1.34 0 .642.472 1.01 1.64 1.284l.987.243c1.838.43 2.596 1.178 2.596 2.53 0 1.72-1.33 2.799-3.453 2.799-1.987 0-3.323-1.029-3.422-2.637z"
+                        fill="white"
+                        fill-rule="nonzero"
+                      ></path>
+                    </svg>
+                  </div>
+                  <div className=" h-8 w-12 p-1">
+                    <svg viewBox="0 0 516 321" fill="none">
+                      <g clip-path="url(#clip0_3:35)">
+                        <path
+                          d="M1.40497 27.0011C-3.73633 14.022 5.84519 0 19.8669 0H106.919C115.098 0 122.342 4.86715 125.381 12.3996L194.671 185.299C196.541 189.935 196.541 195.149 194.671 199.901L151.087 308.484C144.427 325.056 120.823 325.056 114.163 308.484L1.40497 27.0011Z"
+                          fill="url(#paint0_linear_3:35)"
+                        ></path>
+                        <path
+                          d="M169.547 26.4217C164.873 13.5585 174.454 0 188.242 0H264.077C272.49 0 279.968 5.2148 282.772 12.9791L345.753 185.879C347.272 190.166 347.272 194.918 345.753 199.321L307.894 303.27C301.585 320.652 276.813 320.652 270.503 303.27L169.547 26.4217Z"
+                          fill="url(#paint1_linear_3:35)"
+                        ></path>
+                        <path
+                          d="M321.331 27.0011C316.19 14.022 325.771 0 339.793 0H426.845C435.024 0 442.269 4.86715 445.307 12.3996L514.597 185.299C516.467 189.935 516.467 195.149 514.597 199.901L471.013 308.484C464.353 325.056 440.75 325.056 434.089 308.484L321.331 27.0011Z"
+                          fill="url(#paint2_linear_3:35)"
+                        ></path>
+                      </g>
+                      <defs>
+                        <linearGradient
+                          id="paint0_linear_3:35"
+                          x1="7.40492"
+                          y1="55.24"
+                          x2="260.485"
+                          y2="164.437"
+                          gradientUnits="userSpaceOnUse"
+                        >
+                          <stop stop-color="#F213A4"></stop>
+                          <stop offset="0.1517" stop-color="#E011A7"></stop>
+                          <stop offset="0.4554" stop-color="#B20DAF"></stop>
+                          <stop offset="0.8789" stop-color="#6806BB"></stop>
+                          <stop offset="1" stop-color="#5204BF"></stop>
+                        </linearGradient>
+                        <linearGradient
+                          id="paint1_linear_3:35"
+                          x1="175.093"
+                          y1="54.447"
+                          x2="410.968"
+                          y2="148.471"
+                          gradientUnits="userSpaceOnUse"
+                        >
+                          <stop stop-color="#F213A4"></stop>
+                          <stop offset="0.1517" stop-color="#E011A7"></stop>
+                          <stop offset="0.4554" stop-color="#B20DAF"></stop>
+                          <stop offset="0.8789" stop-color="#6806BB"></stop>
+                          <stop offset="1" stop-color="#5204BF"></stop>
+                        </linearGradient>
+                        <linearGradient
+                          id="paint2_linear_3:35"
+                          x1="327.331"
+                          y1="55.24"
+                          x2="580.411"
+                          y2="164.437"
+                          gradientUnits="userSpaceOnUse"
+                        >
+                          <stop stop-color="#F213A4"></stop>
+                          <stop offset="0.1517" stop-color="#E011A7"></stop>
+                          <stop offset="0.4554" stop-color="#B20DAF"></stop>
+                          <stop offset="0.8789" stop-color="#6806BB"></stop>
+                          <stop offset="1" stop-color="#5204BF"></stop>
+                        </linearGradient>
+                        <clipPath id="clip0_3:35">
+                          <rect width="516" height="321" fill="white"></rect>
+                        </clipPath>
+                      </defs>
+                    </svg>
+                  </div>
+                </div>
+              </div>
+              <div className="mt-5">
+                <span className="text-base font-semibold">View live:</span>
+                <div className=" h-8 w-8 rounded-full hover:bg-yellow-500 cursor-pointer">
+                  <Link href="https://cryptopunks-app.vercel.app">
+                    <img
+                      src="https://img.icons8.com/emoji/48/000000/rocket-emji.png"
+                      alt=""
+                    />
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
           <div
-            className="bg-gradient-to-r from-gray-700 to-gray-900 rounded-md shadow-md p-5 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+            className="bg-gradient-to-r from-gray-700 to-gray-900 rounded-md shadow-md p-5 hidden transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
             style={{ height: "280px", width: "400px" }}
           >
             hi
           </div>
           <div
-            className="bg-gradient-to-r from-gray-700 to-gray-900 rounded-md shadow-md p-5 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  hover:bg-yellow-500 duration-300"
+            className="bg-gradient-to-r from-gray-700 to-gray-900 rounded-md shadow-md p-5 hidden transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  hover:bg-yellow-500 duration-300"
             style={{ height: "280px", width: "400px" }}
           >
             hi
